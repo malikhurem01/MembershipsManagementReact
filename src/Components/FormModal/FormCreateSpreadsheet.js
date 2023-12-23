984c7fabbf5c232931a3515e066ee1d6b4955597
Malik <malikhuremovic01@gmail.com>
2023-12-23 16:32:49 +0100

    server routes

--- src/Components/FormModal/FormCreateSpreadsheet.js
+++ src/Components/FormModal/FormCreateSpreadsheet.js
@@ -2,7 +2,15 @@ import React, { useContext, useState } from 'react';
 
 import { useNavigate } from 'react-router-dom';
 
-import { FloatingLabel, Form, Button, Row, Col } from 'react-bootstrap';
+import {
+  FloatingLabel,
+  Form,
+  Button,
+  Row,
+  Col,
+  Modal,
+  Container
+} from 'react-bootstrap';
 
 import loadingSvg from '../../Assets/Pictures/loadingSvg.svg';
 import creationFailed from '../../Assets/Pictures/creationFailed.svg';
@@ -51,130 +59,154 @@ const FormCreateSpreadsheet = ({
 
   return (
     <React.Fragment>
-      <div className={classes.modal}>
+      <Modal
+        show={true}
+        onHide={handleNavigateBack}
+        backdrop="static"
+        keyboard={false}
+        centered
+      >
         <Form onSubmit={handleSubmit}>
-          <h4
-            style={{
-              borderBottom: '1px solid #cecece',
-              marginBottom: '15px',
-              paddingBottom: '5px'
-            }}
-          >
-            Kreiraj bazu
-          </h4>
-          <Row className="g-2">
-            <Col lg={3} md="auto" sm={8}>
-              <FloatingLabel
-                controlId="floatingYear"
-                label="Godina"
-                className="mb-3"
-              >
-                <Form.Control
-                  value={year}
-                  onChange={ev => setYear(ev.target.value)}
-                  type="number"
-                  placeholder="Godina"
-                  label="Baza za godinu:"
-                  required
-                />
-              </FloatingLabel>
-            </Col>
-            <Col lg={3} md="auto" sm={8}>
-              <FloatingLabel
-                controlId="floatingMarriageFee"
-                label="Status brak"
-                className="mb-3"
-              >
-                <Form.Control
-                  value={marriageFee}
-                  onChange={ev => setMarriageFee(ev.target.value)}
-                  type="number"
-                  placeholder="Status brak"
-                  required
-                />
-              </FloatingLabel>
-            </Col>
-            <Col lg={3} md="auto" sm={8}>
-              <FloatingLabel
-                controlId="floatingWidowerFee"
-                label="Status udovac"
-                className="mb-3"
-              >
-                <Form.Control
-                  value={widowerFee}
-                  onChange={ev => setWidowerFee(ev.target.value)}
-                  type="number"
-                  placeholder="Status udovac"
-                  required
-                />
-              </FloatingLabel>
-            </Col>
-            <Col lg={3} md="auto" sm={8}>
-              <FloatingLabel
-                controlId="floatingWidowerFee"
-                label="Granična dob (25 god.)"
-                className="mb-3"
-              >
-                <Form.Control
-                  value={ageLimitFee}
-                  onChange={ev => setAgeLimitFee(ev.target.value)}
-                  type="number"
-                  placeholder="Granična dob"
-                  required
-                />
-              </FloatingLabel>
-            </Col>
-          </Row>
-          {waitingResponse && (
-            <div className={classes.responseModal}>
-              {response.statusCode == null && (
-                <img src={loadingSvg} alt="učitavam kreiranje baze" />
+          {' '}
+          <Modal.Header closeButton>
+            <Modal.Title>Kreiraj bazu</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Container>
+              {waitingResponse && (
+                <React.Fragment>
+                  <div className="d-flex flex-column align-items-center">
+                    {' '}
+                    {response.statusCode == null && (
+                      <img
+                        width={150}
+                        height={150}
+                        src={loadingSvg}
+                        alt="učitavam kreiranje baze"
+                      />
+                    )}
+                    {response.statusCode === 200 && (
+                      <img
+                        width={150}
+                        height={150}
+                        src={creationSuccess}
+                        alt="baza uspješno kreirana"
+                      />
+                    )}
+                    {response.statusCode >= 400 && (
+                      <img
+                        width={150}
+                        height={150}
+                        src={creationFailed}
+                        alt="greška pri kreiranju baze"
+                      />
+                    )}
+                    <h5 className="mt-4">{response.message}</h5>
+                  </div>
+                </React.Fragment>
               )}
-              {response.statusCode === 200 && (
-                <img src={creationSuccess} alt="baza uspješno kreirana" />
+
+              {!waitingResponse && (
+                <React.Fragment>
+                  <p style={{ fontStyle: 'italic' }}>
+                    *Upišite godinu za koju kreirate bazu
+                  </p>
+                  <Row>
+                    <Col lg={12} md="auto" sm={10}>
+                      <FloatingLabel
+                        controlId="floatingYear"
+                        label="Godina"
+                        className="mb-3"
+                      >
+                        <Form.Control
+                          value={year}
+                          onChange={ev => setYear(ev.target.value)}
+                          type="number"
+                          placeholder="Godina"
+                          label="Baza za godinu:"
+                          required
+                        />
+                      </FloatingLabel>
+                    </Col>
+                  </Row>
+                  <p style={{ fontStyle: 'italic' }}>
+                    *Upišite iznos članarine za ispod navedene kategorije
+                  </p>
+                  <Row>
+                    <Col lg={4} md="auto" sm={10}>
+                      <FloatingLabel
+                        controlId="floatingMarriageFee"
+                        label="Status brak"
+                        className="mb-3"
+                      >
+                        <Form.Control
+                          value={marriageFee}
+                          onChange={ev => setMarriageFee(ev.target.value)}
+                          type="number"
+                          placeholder="Status brak"
+                          required
+                        />
+                      </FloatingLabel>
+                    </Col>
+                    <Col lg={4} md="auto" sm={10}>
+                      <FloatingLabel
+                        controlId="floatingWidowerFee"
+                        label="Status udovac"
+                        className="mb-3"
+                      >
+                        <Form.Control
+                          value={widowerFee}
+                          onChange={ev => setWidowerFee(ev.target.value)}
+                          type="number"
+                          placeholder="Status udovac"
+                          required
+                        />
+                      </FloatingLabel>
+                    </Col>
+                    <Col lg={4} md="auto" sm={10}>
+                      <FloatingLabel
+                        controlId="floatingWidowerFee"
+                        label="Granična dob"
+                        className="mb-3"
+                      >
+                        <Form.Control
+                          value={ageLimitFee}
+                          onChange={ev => setAgeLimitFee(ev.target.value)}
+                          type="number"
+                          placeholder="Granična dob"
+                          required
+                        />
+                      </FloatingLabel>
+                    </Col>
+                  </Row>
+                </React.Fragment>
               )}
-              {response.statusCode >= 400 && (
-                <img src={creationFailed} alt="greška pri kreiranju baze" />
+            </Container>
+          </Modal.Body>
+          <Modal.Footer>
+            {waitingResponse &&
+              response?.statusCode >= 400 &&
+              response?.statusCode <= 500 && (
+                <Button variant="danger" onClick={handleNavigateBack}>
+                  Nazad
+                </Button>
               )}
-              <p>{response.message}</p>
-            </div>
-          )}
-          {waitingResponse && response?.statusCode >= 400 && (
-            <Button
-              className={classes.responseButton}
-              onClick={handleClearSubmit}
-              variant="danger"
-            >
-              Poništi
-            </Button>
-          )}
-          {waitingResponse &&
-            response?.statusCode >= 200 &&
-            response?.statusCode < 300 && (
-              <Button
-                className={classes.responseButton}
-                onClick={handleNavigateBack}
-                variant="success"
-              >
-                Nazad
-              </Button>
-            )}
-          {!waitingResponse && (
-            <React.Fragment>
-              <Button
-                style={{ marginRight: '20px' }}
-                type="submit"
-                variant="primary"
-              >
+
+            {!waitingResponse && (
+              <Button type="submit" variant="success">
                 Kreiraj
               </Button>
-              <Button variant="danger" onClick={handleNavigateBack}>
-                Odustani
-              </Button>
-            </React.Fragment>
-          )}
+            )}
+            {waitingResponse &&
+              response?.statusCode >= 200 &&
+              response?.statusCode < 300 && (
+                <Button onClick={handleNavigateBack} variant="success">
+                  Nazad
+                </Button>
+              )}
+          </Modal.Footer>
         </Form>
-      </div>
+      </Modal>
     </React.Fragment>
   );
 };
