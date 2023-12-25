49fb8719b1d87c3100674594d10c6174d7667ed4
Malik <malikhuremovic01@gmail.com>
2023-12-25 21:55:45 +0100

    background and factor date changed

--- src/Templates/DebtWarning/DebtWarningTemplate.js
+++ src/Templates/DebtWarning/DebtWarningTemplate.js
@@ -7,7 +7,6 @@ import robotoBold from '../../Assets/Fonts/Roboto/Roboto-Bold.ttf';
 import robotoMedium from '../../Assets/Fonts/Roboto/Roboto-Medium.ttf';
 
 import styles from './Styles';
-import { factorDate } from '../../Utils/factorDate';
 import { hyphenationCallback } from '../../Utils/hyphenationCallback';
 
 Font.register({
@@ -19,6 +18,18 @@ Font.register({
     { src: robotoMedium, fontStyle: 'medium' }
   ]
 });
+
+const factorDate = () => {
+  const today = new Date();
+  const yyyy = today.getFullYear();
+  let mm = today.getMonth() + 1;
+  let dd = today.getDate();
+
+  if (dd < 10) dd = '0' + dd;
+  if (mm < 10) mm = '0' + mm;
+
+  return dd + '.' + mm + '.' + yyyy;
+};
 
 const DebtWarningTemplate = ({ data }) => {
   return (
