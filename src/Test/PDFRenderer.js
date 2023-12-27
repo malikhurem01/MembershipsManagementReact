328693a2808a710bff32b2d739b86fb17f15aa20
Malik <malikhuremovic01@gmail.com>
2023-12-27 20:10:57 +0100

    blabla

--- src/Test/PDFRenderer.js
+++ src/Test/PDFRenderer.js
@@ -2,7 +2,7 @@ import { PDFViewer } from '@react-pdf/renderer';
 import SpreadsheetReportTemplate from '../Templates/SpreadsheetReport/SpreadsheetReportTemplate';
 import DebtWarningTemplate from '../Templates/DebtWarning/DebtWarningTemplate';
 
-const PDFPage = ({ children }) => {
+const PDFPage = () => {
   return (
     <PDFViewer showToolbar={false} height={777} width={555}>
       <DebtWarningTemplate />
